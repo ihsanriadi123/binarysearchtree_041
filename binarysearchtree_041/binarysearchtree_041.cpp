55834259928a962f0d64b374d19ac495e22ca1fa
ihsan putra riadi <iputrariadi@gmail.com>
2023-06-29 14:46:20 +0700

    Untuk mengulang suatu proses yang belum diketahui jumlahnya

--- binarysearchtree_041/binarysearchtree_041.cpp
+++ binarysearchtree_041/binarysearchtree_041.cpp
@@ -98,3 +98,17 @@ public:
 	}
 };
 
+int main() {
+	BinaryTree obj;
+	while (true) {
+		cout << "\nmenu" << endl;
+		cout << "1. implement insert operation" << endl;
+		cout << "2. perform inorder travesal" << endl;
+		cout << "3. perform preorder travesal" << endl;
+		cout << "4. perform postorder travesal" << endl;
+		cout << "5. Exit" << endl;
+		cout << "\nEnter your choice (1-5) : ";
+
+
+	}
+}
